1908db799401d2a7f090583e326dcf7377943c5b
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-02-01 18:45:05 +0100

    update files

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -6,14 +6,14 @@ import 'firebase/messaging'
 
 
 var config = {
-    apiKey: "AIzaSyDT7K7NoHJEgSPLIcBCJYw8ygy79gaXKuM",
-    authDomain: "react-firebase-auth-58e46.firebaseapp.com",
-    databaseURL: "https://react-firebase-auth-58e46.firebaseio.com",
-    projectId: "react-firebase-auth-58e46",
-    storageBucket: "react-firebase-auth-58e46.appspot.com",
-    messagingSenderId: "620107259945",
-    appId: "1:620107259945:web:dfbd683426fa48b3f8bc38",
-    measurementId: "G-57SCLFW9RH"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(config) ;
